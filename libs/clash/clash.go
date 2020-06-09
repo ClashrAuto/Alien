f92d57069bb749f11ee7648169c41db9c9963eef
Jack Wang <jack.wang@integem.com>
2020-06-10 00:13:12 +0800

    优化clash使用最新的code集成ssr协议

--- libs/clash/clash.go
+++ libs/clash/clash.go
@@ -7,60 +7,92 @@ import (
 	"path/filepath"
 	"runtime"
 
-	"github.com/ClashrAuto/Clashr/config"
-	C "github.com/ClashrAuto/Clashr/constant"
-	"github.com/ClashrAuto/Clashr/hub"
-
-	log "github.com/sirupsen/logrus"
+	"github.com/ClashrAuto/clash/config"
+	"github.com/ClashrAuto/clash/constant"
+	C "github.com/ClashrAuto/clash/constant"
+	"github.com/ClashrAuto/clash/hub"
+	"github.com/ClashrAuto/clash/hub/executor"
+	"github.com/ClashrAuto/clash/log"
 )
 
 var (
-	version bool
-	homedir string
+	flagset            map[string]bool
+	version            bool
+	testConfig         bool
+	homeDir            string
+	configFile         string
+	externalUI         string
+	externalController string
+	secret             string
 )
 
 func init() {
-	flag.StringVar(&homedir, "d", "", "set configuration directory")
+	flag.StringVar(&homeDir, "d", "", "set configuration directory")
+	flag.StringVar(&configFile, "f", "", "specify configuration file")
+	flag.StringVar(&externalUI, "ext-ui", "", "override external ui directory")
+	flag.StringVar(&externalController, "ext-ctl", "", "override external controller address")
+	flag.StringVar(&secret, "secret", "", "override secret for RESTful API")
 	flag.BoolVar(&version, "v", false, "show current version of clash")
+	flag.BoolVar(&testConfig, "t", false, "test configuration and exit")
 	flag.Parse()
-}
 
-func error(e string) {
-	log.Fatalf("Error: %s", e)
+	flagset = map[string]bool{}
+	flag.Visit(func(f *flag.Flag) {
+		flagset[f.Name] = true
+	})
 }
 
 func Clash() {
-	// defer func() {
-	// 	if err := recover(); err != nil {
-	// 		fmt.Println("panic recover")
-	// 		go main()
-	// 	}
-	// }()
-
 	if version {
 		fmt.Printf("Clash %s %s %s %s\n", C.Version, runtime.GOOS, runtime.GOARCH, C.BuildTime)
-		os.Exit(0)
+		return
 	}
 
-	if homedir != "" {
-		if !filepath.IsAbs(homedir) {
+	if homeDir != "" {
+		if !filepath.IsAbs(homeDir) {
 			currentDir, _ := os.Getwd()
-			homedir = filepath.Join(currentDir, homedir)
+			homeDir = filepath.Join(currentDir, homeDir)
 		}
-		C.SetHomeDir(homedir)
+		C.SetHomeDir(homeDir)
+	}
+
+	if configFile != "" {
+		if !filepath.IsAbs(configFile) {
+			currentDir, _ := os.Getwd()
+			configFile = filepath.Join(currentDir, configFile)
+		}
+		C.SetConfig(configFile)
+	} else {
+		configFile := filepath.Join(C.Path.HomeDir(), C.Path.Config())
+		C.SetConfig(configFile)
 	}
 
 	if err := config.Init(C.Path.HomeDir()); err != nil {
-		log.Fatalf("Initial configuration directory error: %s", err.Error())
-		// error(err.Error())
+		log.Fatalln("Initial configuration directory error: %s", err.Error())
 	}
 
-	if err := hub.Parse(); err != nil {
-		log.Fatalf("Parse config error: %s", err.Error())
-		// error(err.Error())
+	if testConfig {
+		if _, err := executor.Parse(); err != nil {
+			log.Errorln(err.Error())
+			fmt.Printf("configuration file %s test failed\n", constant.Path.Config())
+			os.Exit(1)
+		}
+		fmt.Printf("configuration file %s test is successful\n", constant.Path.Config())
+		return
 	}
 
-	// sigCh := make(chan os.Signal, 1)
-	// signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	// <-sigCh
+	var options []hub.Option
+	if flagset["ext-ui"] {
+		options = append(options, hub.WithExternalUI(externalUI))
+	}
+	if flagset["ext-ctl"] {
+		options = append(options, hub.WithExternalController(externalController))
+	}
+	if flagset["secret"] {
+		options = append(options, hub.WithSecret(secret))
+	}
+
+	if err := hub.Parse(options...); err != nil {
+		log.Fatalln("Parse config error: %s", err.Error())
+	}
 }
